ce6e33c6cfe13502936280890109b611615f2561
Peter Larsson <Peter.Eslov@hotmail.se>
2026-06-25 10:50:39 +0200

    Test webbhook trigger

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const port = 80
 
 const server = http.createServer((request, response) => {
-    const content = fs.readFileSync('./hello.txt', 'utf8')
+    const content = fs.readFileSync('src/hello.txt', 'utf8')
     response.writeHead(200, { 'Content-Type': 'text/plain' })
     response.write(content)
     response.end('Version: ' + process.env.NODE_VERSION + '\n')
